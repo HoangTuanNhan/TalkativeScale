d1575c4beb5b84d535e0329a2690e49d8958c39b
HoangTuanNhan <hoangtuannhanbk@gmail.com>
2016-10-06 14:54:27 +0700

    First Commit

---
+++ WebContent/test.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Talkative Scale</title>
+</head>
+<body>
+	<div>
+		<img src="<%=request.getContextPath() %>/image/storm.png">
+		<h2>STORM TEAM</h2>
+	</div>
+	<%
+		Float weight=(Float)request.getAttribute("weight");
+	%>
+	<div>
+		<p><%=weight %></p>
+	</div>
+</body>
+</html>
---
+++ src/action/GetApi.java
@@ -0,0 +1,38 @@
+package action;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetApi
+ */
+public class GetApi extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetApi() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/api/getApi.jsp").forward(request, response);;
+	}
+
+}
